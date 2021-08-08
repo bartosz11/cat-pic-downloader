a7fe78dfe827b8c58fe34e2705764ba5110c23f8
bartosz11 <86555598+bartosz11@users.noreply.github.com>
2021-08-08 12:30:08 +0200

    done?

--- src/main/java/me/bartosz1/catdownloader/CatDownloader.java
+++ src/main/java/me/bartosz1/catdownloader/CatDownloader.java
@@ -1,9 +1,30 @@
 package me.bartosz1.catdownloader;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class CatDownloader {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many cat pictures you want to download? Choose between 1 and 2147483467.");
+        int cats=0;
+        try {
+            cats = scanner.nextInt();
+        } catch (InputMismatchException e){
+            System.out.println("Too big amount!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        if (cats < 1){
+            System.out.println("Too small amount!");
+            System.exit(1);
+        }
+        scanner.close();
+        for (int i=0; i<cats; i++){
+            new DownloadThread(i+1+".png").run();
+        }
     }
 
 }
